b0da33c47a379ff89d21e724c9ed7995698d8f57
Cosmo <htam@student.42berlin.de>
2024-03-12 19:53:05 +0100

    1171

--- leetcode/problems/1171_remove_zero_sum_consecutive_nodes_from_linked_list.cpp
+++ leetcode/problems/1171_remove_zero_sum_consecutive_nodes_from_linked_list.cpp
@@ -13,10 +13,62 @@ struct ListNode {
   ListNode(int x) : val(x), next(nullptr) {}
   ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
- 
+
+
 class Solution {
 public:
     ListNode* removeZeroSumSublists(ListNode* head) {
-        
+      ListNode* to_remove = checkSublist(head);
+      if (!to_remove)
+        return (head);
+    
+      removeSublist(&head, to_remove);
+      head = removeZeroSumSublists(head);
+      return (head);
+    }
+
+    ListNode* checkSublist(ListNode* head) {
+      if (!head)
+        return (nullptr);
+      ListNode* current = head;
+      int sum = 0;
+
+      while (current) {
+        sum += current->val;
+        current = current->next;
+        if (sum == 0)
+          return head;
+      }
+      return (checkSublist(head->next));
+    }
+
+    ListNode* remove(ListNode* node) {
+      ListNode* temp;
+      int sum = 0;
+
+      while (node) {
+        sum += node->val;
+        temp = node;
+        node = node->next;
+        // delete temp;
+        if (sum == 0)
+          break;
+      }
+      return node;
+    }
+
+    void removeSublist(ListNode** head, ListNode* to_remove) {
+      
+      if (*head == to_remove) {
+        *head = remove(*head);
+        return ;
+      }
+
+      ListNode* current = *head;
+      while (current->next && current->next != to_remove)
+        current = current->next;
+
+      current->next = remove(current->next);
+      return ;
     }
 };
